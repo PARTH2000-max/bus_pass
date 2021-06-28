2b0c348745c4c5e6b15221ba011a2567eef9528a
rajkayasth <rajkayasth19@gmail.com>
2021-06-28 22:28:07 +0530

    View pass Rate Intent pass And created OnViewCreated Method on Home Fregment

--- app/src/main/java/com/example/buspass/HomeFragment.java
+++ app/src/main/java/com/example/buspass/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.buspass;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import org.jetbrains.annotations.NotNull;
@@ -24,6 +26,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class HomeFragment extends Fragment {
+    private CardView Card_passRate;
 
    /* TextView tv;
     EditText ed;
@@ -41,11 +44,26 @@ public class HomeFragment extends Fragment {
 
         tv.setText("");
         process();
-*/
+        */
+
     }
 
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-   /* public void process()
+        Card_passRate = view.findViewById(R.id.Card_passRate);
+
+        Card_passRate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getActivity().getApplication(),PassRates_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    /* public void process()
     {
         Retrofit retrofit=new Retrofit.Builder()
                 .baseUrl(url)
