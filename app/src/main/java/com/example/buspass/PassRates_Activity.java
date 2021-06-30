ad99654385b4c106f88a89b6ff9dad9b06f7c891
rajkayasth <rajkayasth19@gmail.com>
2021-06-30 22:02:36 +0530

    View pass Rate Intent pass And created OnViewCreated Method on Home Fregment

--- app/src/main/java/com/example/buspass/PassRates_Activity.java
+++ app/src/main/java/com/example/buspass/PassRates_Activity.java
@@ -1,14 +1,29 @@
 package com.example.buspass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class PassRates_Activity extends AppCompatActivity {
+
+    private RecyclerView rcPassList;
+    private ArrayList<PassRate_Model> Passrate_model_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pass_rates);
+
+        rcPassList = findViewById(R.id.RCPassRates);
+
+        Passrate_model_list = new ArrayList<PassRate_Model>();
+
+
+        Passrate_model_list.add(new PassRate_Model("AMTS",R.drawable.bg_image));
+
+
     }
 }
